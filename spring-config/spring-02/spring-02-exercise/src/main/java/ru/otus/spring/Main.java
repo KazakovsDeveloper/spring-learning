2d16b6ab8c8693c03ee932b7e3f01ca6f95e5ca0
Maxim Kazakov <m.c.kazakov.developer@gmail.com>
2023-08-17 18:18:46 +0300

    Add repo for spring conf

---
+++ spring-config/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/Main.java
@@ -0,0 +1,20 @@
+package ru.otus.spring;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.otus.spring.domain.Person;
+import ru.otus.spring.service.PersonService;
+
+/**
+ * вспоминаем аннотации для конфигурации java-based + annotation
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = null;
+
+        PersonService service = context.getBean(PersonService.class);
+
+        Person ivan = service.getByName("Ivan");
+        System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
+    }
+}
