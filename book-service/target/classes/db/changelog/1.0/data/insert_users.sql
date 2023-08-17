352429110927b7bdaab7e5e3a0407f5e00a5dc56
Maxim Kazakov <m.c.kazakov.developer@gmail.com>
2023-08-17 18:59:22 +0300

    Finish commit

--- book-service/target/classes/db/changelog/1.0/data/insert_users.sql
+++
@@ -1,4 +0,0 @@
--- file: insert_users.sql
-
-INSERT INTO users (login, role, password) VALUES ('user1', 'ROLE_ADMIN', 'password1');
-INSERT INTO users (login, role, password) VALUES ('user2', 'ROLE_USER', 'password2');
